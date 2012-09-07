984f1fa0f4896b89cb6e1dc3a38508db7a20f7a5
sukso96100 <sukso96100@gmail.com>
2012-09-07 22:31:52 +0900

    Added ListView on "Home" Activity
    -Files that Modified by me:
    mobileilsin/res/values/strings.xml
    mobileilsin/res/layout/activity_home.xml
    mobileilsin/src/com/iadev/Home.java
    
    -Other Files were automatically modified or generated

--- mobileilsin/src/com/iadev/mobileilsin/Home.java
+++ mobileilsin/src/com/iadev/mobileilsin/Home.java
@@ -1,16 +1,35 @@
 package com.iadev.mobileilsin;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Home extends Activity {
+	ListView list;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        
+        list = (ListView)findViewById(R.id.listView1);
+        
+        ArrayList<String> dataArr = new ArrayList<String>();
+        dataArr.add("공지사항");
+        dataArr.add("점심메뉴");
+        dataArr.add("학교정보");
+        dataArr.add("앱 정보");
+
+        ArrayAdapter<String> Adapter = new ArrayAdapter<String>  (this, android.R.layout.simple_list_item_1, dataArr);
+
+        list.setAdapter(Adapter);
     }
+
+    
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
