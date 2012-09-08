335718b2210bbff64a5a96908657d7a95f94840d
sukso96100 <sukso96100@gmail.com>
2012-09-08 16:31:16 +0900

    Changed Activity type: Avtivity -> ListActivity

--- mobileilsin/src/com/iadev/mobileilsin/Home.java
+++ mobileilsin/src/com/iadev/mobileilsin/Home.java
@@ -1,41 +1,35 @@
+
+
 package com.iadev.mobileilsin;
 
 import java.util.ArrayList;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class Home extends Activity {
-	ListView list;
+public class Home extends ListActivity {
+  public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    String[] values = new String[] { "공지사항", "점심메뉴", "학교정보",
+        "앱 정보" };
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, values);
+    setListAdapter(adapter);
+  }
+  
+  @Override
+  public void onListItemClick(ListView l, View v, int position, long id) {
+	  Toast.makeText(getApplicationContext(),
+		      "Click ListItem Number " + position, Toast.LENGTH_LONG)
+		      .show();
+		  }
+		 
+    // Do something with the data
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        
-        ListView listView = (ListView) findViewById(R.id.listView1);
-        String[] values = new String[] { "공지사항", "점심메뉴", "학교정보",
-          "앱 정보"};
-
-        // First paramenter - Context
-        // Second parameter - Layout for the row
-        // Third parameter - ID of the TextView to which the data is written
-        // Forth - the Array of data
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-          android.R.layout.simple_list_item_1, android.R.id.text1, values);
-
-        // Assign adapter to ListView
-        listView.setAdapter(adapter); 
-    }
-
-    
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_home, menu);
-        return true;
-    }
-}
+  
+  
+} 
