3e75ad5832065c5faa3bcd8cc144f9b2e291d1fc
sukso96100 <sukso96100@gmail.com>
2012-09-12 17:41:18 +0900

    Added some function and fixed some problem

--- mobileilsin/src/com/iadev/mobileilsin/Appinfo.java
+++ mobileilsin/src/com/iadev/mobileilsin/Appinfo.java
@@ -1,20 +1,52 @@
 package com.iadev.mobileilsin;
 
+
+
+import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.view.View;
 
-public class Appinfo extends Activity {
+public class Appinfo extends ListActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_appinfo);
+  
+
+
+
+	public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        String[] values = new String[] { "앱 버전", "앱 개발자", "동아리 홈페이지 방문",
+            "플레이 스토어에서 업데이트 확인", "앱 소개" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, values);
+            setListAdapter(adapter);
+          }
+    
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		  super.onListItemClick(l, v, position, id);
+  	  
+  	  if(position == 2){
+  		Intent intent = new Intent();
+        
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://iadev.3owl.com"));
+        startActivity(intent);
+  	  }
+  	  
+  	  else if(position == 3){
+  	  Intent intent1 = new Intent( Appinfo.this, Launchmenu.class);
+  	  startActivity(intent1);
+  	  }
+  	  
+  	  else if(position == 4){
+  	  Intent intent2 = new Intent( Appinfo.this, Schoolinfo.class);
+  	  startActivity(intent2);
+  	  }
+  	  
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_appinfo, menu);
-        return true;
-    }
+    
+    
 }
--- mobileilsin/src/com/iadev/mobileilsin/Home.java
+++ mobileilsin/src/com/iadev/mobileilsin/Home.java
@@ -10,7 +10,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class Home extends ListActivity {
   public void onCreate(Bundle icicle) {
