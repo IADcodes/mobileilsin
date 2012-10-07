ee34ba9d9edc092fd21f6fec3ce2403c93cc52ba
gt10034 <gt10034@naver.com>
2012-10-07 17:06:10 +0900

    Added Notification function on "Notification" Activity
    Using Tiny Rss Library to show content since school homepage doesn't
    have Rss Feed yet, This function isn't work yet
    
    Tiny Rss Library:
    https://github.com/IADcodes/TinyRss

--- mobileilsin/src/com/iadev/mobileilsin/Notification.java
+++ mobileilsin/src/com/iadev/mobileilsin/Notification.java
@@ -3,18 +3,12 @@ package com.iadev.mobileilsin;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.*;
 
-public class Notification extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notification);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_notification, menu);
-        return true;
-    }
+public class Notification extends com.tinymission.rss.FeedActivity
+ {
+	@Override
+	public String getFeedUrl() {
+		return "[]";
+	}
 }
