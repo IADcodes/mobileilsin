879a4b4fb232e16467375be75998bf6c77fbf33f
sukso96100 <sukso96100@gmail.com>
2012-10-20 14:27:25 +0900

    Using Webwiew instead of using html parsing Reason : it is too hard for me to parse web page and it takes many time to parse web page

--- mobileilsin/gen/com/iadev/mobileilsin/R.java
+++ mobileilsin/gen/com/iadev/mobileilsin/R.java
@@ -20,7 +20,7 @@ public final class R {
         public static final int menu_settings=0x7f070005;
         public static final int textView1=0x7f070003;
         public static final int textView2=0x7f070004;
-        public static final int tv1=0x7f070002;
+        public static final int webView1=0x7f070002;
     }
     public static final class layout {
         public static final int activity_appinfo=0x7f030000;
--- mobileilsin/src/com/iadev/mobileilsin/Notification.java
+++ mobileilsin/src/com/iadev/mobileilsin/Notification.java
@@ -1,58 +1,53 @@
 package com.iadev.mobileilsin;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import net.htmlparser.jericho.Source;
-
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-
-
-public class Notification extends Activity
- {
-	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_notification);
-
-	String yourUrl = "http://bcilsin.ms.kr/?act=news.main&comm=com";
-
-	TextView tv = (TextView)findViewById(R.id.tv1);
-	tv.setText(getHtmltoText(yourUrl));
-	}
-
-	public String getHtmltoText(String sourceUrlString) {
-	Source source = null;
-	String content = null;
-
-	try {
-	//url에서 html 소스를 읽어온다.
-		URL sUrl = new URL(sourceUrlString);
-		InputStream is = sUrl.openStream();
-		Source source1= new Source(new InputStreamReader(is,"utf-8"));
-	// 한글사용을 위해서는 윗 줄의 코드를 아래처럼 수정
-	// URL sUrl = new URL(sourceUrlString);
-	// InputStream is = sUrl.openStream();
-	// Source source= new Source(new InputStreamReader(is,"euc-kr"));
-	// "euc-kr" 부분은 charset에 맞춰 변경해준다. UTF-8, KSC5601 etc...
-
-	// 전체 소스 구문을 분석한다.
-	source1.fullSequentialParse();
-	// HTML markup에서 text contents만 가져와서 스트링으로 변환한다.
-	content=source1.getTextExtractor().toString();
-	} catch (Exception e) {
-	e.printStackTrace();
-	}
-	return content;
-	}
-	}
-
-
-
-
-	
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+ 
+public class Notification extends Activity{
+     
+    private WebView mWebView;
+     
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+         
+        setLayout();
+         
+        // 웹뷰에서 자바스크립트실행가능
+        mWebView.getSettings().setJavaScriptEnabled(true); 
+        // 구글홈페이지 지정
+        mWebView.loadUrl("http://bcilsin.ms.kr/?act=news.main&comm=com");
+        // WebViewClient 지정
+        mWebView.setWebViewClient(new WebViewClientClass());  
+         
+    }
+     
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) { 
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) { 
+            mWebView.goBack(); 
+            return true; 
+        } 
+        return super.onKeyDown(keyCode, event);
+    }
+     
+    private class WebViewClientClass extends WebViewClient { 
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) { 
+            view.loadUrl(url); 
+            return true; 
+        } 
+    }
+     
+    /*
+     * Layout
+     */
+    private void setLayout(){
+        mWebView = (WebView) findViewById(R.id.webView1);
+    }
+}
