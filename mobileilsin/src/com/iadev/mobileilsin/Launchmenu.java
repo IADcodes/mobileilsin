bebf57dbdbfe580f348e4ec8e763390001ea22db
sukso96100 <sukso96100@gmail.com>
2012-10-20 14:49:47 +0900

    Updated app version info

--- mobileilsin/src/com/iadev/mobileilsin/Launchmenu.java
+++ mobileilsin/src/com/iadev/mobileilsin/Launchmenu.java
@@ -1,20 +1,54 @@
 package com.iadev.mobileilsin;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class Launchmenu extends Activity {
 
+public class Launchmenu extends Activity{
+     
+    private WebView mWebView;
+     
+ 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launchmenu);
+         
+        setLayout();
+         
+        // 웹뷰에서 자바스크립트실행가능
+        mWebView.getSettings().setJavaScriptEnabled(true); 
+        // 구글홈페이지 지정
+        mWebView.loadUrl("http://bcilsin.ms.kr/?act=news.main&comm=com");
+        // WebViewClient 지정
+        mWebView.setWebViewClient(new WebViewClientClass());  
+         
     }
-
+     
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_launchmenu, menu);
-        return true;
+    public boolean onKeyDown(int keyCode, KeyEvent event) { 
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) { 
+            mWebView.goBack(); 
+            return true; 
+        } 
+        return super.onKeyDown(keyCode, event);
+    }
+     
+    private class WebViewClientClass extends WebViewClient { 
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) { 
+            view.loadUrl(url); 
+            return true; 
+        } 
+    }
+     
+    /*
+     * Layout
+     */
+    private void setLayout(){
+        mWebView = (WebView) findViewById(R.id.webView1);
     }
 }
